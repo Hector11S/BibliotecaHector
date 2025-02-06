0243831b564c6386a7074e02bf2963e1e897cc3c
Hector11S <161340531+Hector11S@users.noreply.github.com>
2025-02-05 22:25:27 -0600

    Feat(carpeta de proyecto)

---
+++ ProyectoLibreria/api/Practica.API/Extensiones/MappingProfileExtensions.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Entities.Entities;
+using Practica.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica.API.Extensiones
+{
+    public class MappingProfileExtensions: Profile
+    {
+        public MappingProfileExtensions()
+        {
+    
+            CreateMap<LibroViewModel, tbLibros>().ReverseMap();
+     
+
+        }
+    }
+}
+
+
+
---
+++ ProyectoLibreria/api/Practica.API/Startup.cs
@@ -0,0 +1,100 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Practica.API.Extensiones;
+using Practica.API.Servicios;
+using Practica.BussinesLogic.Servicios;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+ 
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<MailSettings>(Configuration.GetSection("MailSettings"));
+
+            services.AddTransient<IMailService, MailService>();
+            services.AddAutoMapper(x => x.AddProfile<MappingProfileExtensions>(), AppDomain.CurrentDomain.GetAssemblies());
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PracticaAPI.API", Version = "v1" });
+            });
+            services.DataAcces(Configuration.GetConnectionString("PracticaAPIConn"));
+            services.BussinesLogic();
+            services.AddAutoMapper(typeof(Startup));
+            services.AddHttpContextAccessor();
+            services.AddDistributedMemoryCache();
+            services.AddSession(options => {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyHeader()
+                               .AllowAnyMethod();
+                    });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PracticaAPI.API v1"));
+
+            app.UseSession();
+            app.UseCors("AllowSpecificOrigin");
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                  Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads")),
+                RequestPath = "/uploads"
+            });
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
